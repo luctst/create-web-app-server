5ab5803429493f3e7e5292a14dcce15b4c9d801e
Lucas Tostée <lucas.tostee@gmail.com>
2019-05-03 02:30:35 +0200

    Start nextJs config for SSR

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => (
+    <>
+        <Head>
+            <title key="title">Create Web App Server</title>
+        </Head>
+        <h1>Hello World</h1>
+    </>
+);
+
+export default Home;
